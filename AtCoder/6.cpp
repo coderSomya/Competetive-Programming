cad3da0a4b26793ce9321211d451fefb35a30a28
Somya <isomya13@gmail.com>
2023-03-02 00:06:44 +0530

    case work

---
+++ AtCoder/6.cpp
@@ -0,0 +1,62 @@
+/*
+https://atcoder.jp/contests/arc157/tasks/arc157_a
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i]
+;
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+  
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+ 
+void solve(){
+    int n,xx,xy,yx,yy;
+    cin>>n>>xx>>xy>>yx>>yy;
+    
+    int r= abs(xy-yx);
+
+    switch(r){
+        case 0:
+           if(xy==0){
+            if(yy==0 || xx==0){
+                cout<<"Yes"<<endl;
+            }
+            else cout<<"No"<<endl;
+           }
+           else cout<<"Yes"<<endl;
+           break;
+        case 1:
+          cout<<"Yes"<<endl;
+          break;
+        default:
+           cout<<"No"<<endl;
+           break;
+    }
+
+}
+  
+  
+int32_t main(){
+// int t;
+// cin>>t;
+// while(t--) 
+solve();
+return 0;
+}
