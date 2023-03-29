7181b962ccc876656e2f9f4a230fa9f0479682ab
Somya <isomya13@gmail.com>
2023-03-29 20:39:29 +0530

    easy

---
+++ codeforces/interestingStory.cpp
@@ -0,0 +1,72 @@
+/*
+problem link ->
+https://codeforces.com/problemset/problem/1551/C
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i]
+;
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+  
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+//try to make each letter max 
+void solve(){
+    
+    take_n
+    vector<string> v;
+    loop(i,0,n){
+        string s; cin>>s; v.pb(s);
+    }
+
+    int ans=0;
+
+    loop(i,0,5){
+
+        int target='a'+i;
+        vi p;
+        loop(i,0,n){
+            int x=0,y=0;
+            for(int j=0; j<v[i].length(); j++){
+              x+= v[i][j]==target;
+              y+= v[i][j]!=target;
+            }
+            p.pb(x-y);
+        }
+        sort(p.begin(), p.end());
+        int ct=0; int o=0;
+        for(int m=n-1; m>=0; m--){
+           o+=p[m];
+           if(o>0) ct++;
+           else break;
+        }
+        ans=max(ans,ct);
+    }
+
+    cout<<ans<<endl;
+
+}
+  
+  
+int32_t main(){
+int t;
+cin>>t;
+while(t--) solve();
+return 0;
+}
