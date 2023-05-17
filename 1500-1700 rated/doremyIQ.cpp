634aace96b555f6683ef9666e6829ca8d494fc42
Somya <isomya13@gmail.com>
2023-05-17 12:40:14 +0530

    prolly correct

---
+++ 1500-1700 rated/doremyIQ.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, q;
+vector<int> arr;
+string s;
+int dp[(int)1e5+5];
+
+int check(int i ,int curr){ //returns max contests from pos=i, and strength=curr
+
+    //base case
+    if(i==n) return 0;
+
+    if(curr<=0) return 0;
+
+    //cache check
+    if(dp[i]!=-1) return dp[i];
+
+    //transition
+    if(arr[i]<=curr){
+        return dp[i]= 1+check(i+1, curr);
+    }
+    
+    else{
+        return dp[i]= max(check(i+1,curr), 1+check(i+1, curr-1));
+    }
+
+
+}
+
+void solve(){
+
+    cin>>n>>q;
+
+    arr.clear(); arr.resize(n);
+    memset(dp, -1, sizeof(dp));
+    for(int i=0;i<n; i++) cin>>arr[i];
+
+    int ans=check(0,q);
+
+    string res="";
+    int p=1;
+    for(int i=n-1; i>=0; i--){
+        if(dp[i]==p){
+            res+="1";
+            p++;
+        }
+        else{
+            res+="0";
+        }
+    
+    }
+
+    reverse(res.begin(), res.end());
+
+
+    cout<<res<<endl;
+
+}
+
+int main(){
+    int t; cin>>t;
+    while(t--) solve();
+
+
+    return 0;
+}
